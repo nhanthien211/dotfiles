aac27cb2d702352b6b79b1347198be2d47d7b0b3
Tran Thien Nhan <ttn@netcompany.com>
2024-03-26 16:08:26 +0700

    Add telescope find in hidden feature

--- neovim/lua/plugins/editor/telescope.lua
+++ neovim/lua/plugins/editor/telescope.lua
@@ -15,6 +15,13 @@ return {
 			local telescope = require("telescope")
 			telescope.load_extension("fzf")
 
+			local telescopeConfig = require("telescope.config")
+
+			local vimgrep_arguments = { table.unpack(telescopeConfig.values.vimgrep_arguments) }
+			table.insert(vimgrep_arguments, "--hidden")
+			table.insert(vimgrep_arguments, "--glob")
+			table.insert(vimgrep_arguments, "!**/.git/*")
+
 			local actions = require("telescope.actions")
 			telescope.setup({
 				pickers = {
@@ -22,11 +29,12 @@ return {
 						enable_preview = true,
 					},
 					find_files = {
-						hidden = true,
+						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 					},
 				},
 				defaults = {
 					wrap_results = true,
+					vimgrep_arguments = vimgrep_arguments,
 					mappings = {
 						i = {
 							["<esc>"] = actions.close,
