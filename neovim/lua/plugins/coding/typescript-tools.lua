66691c33a744497801db849264c60b0f5c896605
Tran Thien Nhan <ttn@netcompany.com>
2024-04-11 14:45:59 +0700

    Add pre-setting for inlay, waiting for 0.10 stable

--- neovim/lua/plugins/coding/typescript-tools.lua
+++ neovim/lua/plugins/coding/typescript-tools.lua
@@ -1,6 +1,30 @@
 return {
 	{
 		"pmizio/typescript-tools.nvim",
-		opts = {},
+		config = function()
+			require("typescript-tools").setup({
+				-- on_attach = function(client, bufnr)
+				-- 	client.server_capabilities.documentFormattingProvider = false
+				-- 	client.server_capabilities.documentRangeFormattingProvider = false
+				--
+				-- 	if vim.lsp.inlay_hint then
+				-- 		vim.lsp.inlay_hint.enable(bufnr, true)
+				-- 	end
+				-- end,
+				-- settings = {
+				-- 	tsserver_file_preferences = {
+				-- 		-- Inlay Hints
+				-- 		includeInlayParameterNameHints = "all",
+				-- 		includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				-- 		includeInlayFunctionParameterTypeHints = true,
+				-- 		includeInlayVariableTypeHints = true,
+				-- 		includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+				-- 		includeInlayPropertyDeclarationTypeHints = true,
+				-- 		includeInlayFunctionLikeReturnTypeHints = true,
+				-- 		includeInlayEnumMemberValueHints = true,
+				-- 	},
+				-- },
+			})
+		end,
 	},
 }
