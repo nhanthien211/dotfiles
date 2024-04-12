1c69a9d88a729dde66a946431c4f8f5c031875a5
Tran Thien Nhan <ttn@netcompany.com>
2024-04-12 15:33:13 +0700

    Update setting

--- neovim/lua/plugins/coding/autopairs.lua
+++ neovim/lua/plugins/coding/autopairs.lua
@@ -2,42 +2,28 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		lazy = true,
-    event = "InsertEnter",
+		event = "InsertEnter",
 		config = true,
 	},
 
 	{
 		"echasnovski/mini.surround",
-		lazy = true,
-    version = false,
-		keys = function(_, keys)
-			-- Populate the keys based on the user's options
-			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-			local mappings = {
-				{ opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-				{ opts.mappings.delete, desc = "Delete surrounding" },
-				{ opts.mappings.find, desc = "Find right surrounding" },
-				{ opts.mappings.find_left, desc = "Find left surrounding" },
-				{ opts.mappings.highlight, desc = "Highlight surrounding" },
-				{ opts.mappings.replace, desc = "Replace surrounding" },
-				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-			}
-			mappings = vim.tbl_filter(function(m)
-				return m[1] and #m[1] > 0
-			end, mappings)
-			return vim.list_extend(mappings, keys)
+		version = "*",
+		config = function()
+			require("mini.surround").setup({
+				mappings = {
+					add = "sa", -- Add surrounding in Normal and Visual modes
+					delete = "sd", -- Delete surrounding
+					find = "sf", -- Find surrounding (to the right)
+					find_left = "sF", -- Find surrounding (to the left)
+					highlight = "sh", -- Highlight surrounding
+					replace = "sr", -- Replace surrounding
+					update_n_lines = "sn", -- Update `n_lines`
+
+					suffix_last = "l", -- Suffix to search with "prev" method
+					suffix_next = "n", -- Suffix to search with "next" method
+				},
+			})
 		end,
-		opts = {
-			mappings = {
-				add = "gsa",
-				delete = "gsd",
-				find = "gsf",
-				find_left = "gsF",
-				highlight = "gsh",
-				replace = "gsr",
-				update_n_lines = "gsn",
-			},
-		},
 	},
 }
--- neovim/lua/plugins/editor/flash.lua
+++ neovim/lua/plugins/editor/flash.lua
@@ -4,17 +4,17 @@ return {
 		event = "VeryLazy",
 		vscode = true,
 		opts = {
-			-- modes = {
-			-- 	search = {
-			-- 		highlight = {
-			-- 			backdrop = true, -- enable for
-			-- 		},
-			-- 	},
-			-- },
+			modes = {
+				treesitter = {
+					highlight = {
+						backdrop = true,
+					},
+				},
+			},
 		},
 		keys = {
 			{
-				"s",
+				"<leader>s",
 				mode = { "n", "x", "o" },
 				function()
 					require("flash").jump()
@@ -22,37 +22,37 @@ return {
 				desc = "Flash",
 			},
 			{
-				"S",
+				"<leader>S",
 				mode = { "n", "x", "o" },
 				function()
 					require("flash").treesitter()
 				end,
 				desc = "Flash Treesitter",
 			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-			{
-				"R",
-				mode = { "o", "x" },
-				function()
-					require("flash").treesitter_search()
-				end,
-				desc = "Treesitter Search",
-			},
-			{
-				"<c-s>",
-				mode = { "c" },
-				function()
-					require("flash").toggle()
-				end,
-				desc = "Toggle Flash Search",
-			},
+			-- {
+			-- 	"r",
+			-- 	mode = "o",
+			-- 	function()
+			-- 		require("flash").remote()
+			-- 	end,
+			-- 	desc = "Remote Flash",
+			-- },
+			-- {
+			-- 	"R",
+			-- 	mode = { "o", "x" },
+			-- 	function()
+			-- 		require("flash").treesitter_search()
+			-- 	end,
+			-- 	desc = "Treesitter Search",
+			-- },
+			-- {
+			-- 	"<c-s>",
+			-- 	mode = { "c" },
+			-- 	function()
+			-- 		require("flash").toggle()
+			-- 	end,
+			-- 	desc = "Toggle Flash Search",
+			-- },
 		},
 	},
 }
