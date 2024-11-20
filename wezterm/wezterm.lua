dbf3bbac82e2c994548fe131ee554f53ee9ccbbe
Nhan Tran <ttn2101@gmail.com>
2024-11-20 14:53:25 +0700

    Rework structure and add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local keys = {}
+
+-- Main configuration
+config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font('MesloLGS NF', { weight = 'Bold' })
+config.font_size = 13
+config.enable_tab_bar = false
+
+config.background= {
+    -- the back-most layer, should be image
+    {
+      source = {
+        File = '/Users/ttn/.config/wezterm/wallpaper/neovim-background-2.jpg', -- Replace with the path to your image
+      },
+      opacity = 1.0, -- Adjust transparency (1.0 is fully opaque, 0.0 is fully transparent)
+    },
+    -- follow by a color layer which on top of the previous image
+    -- set opacity so we can see the image 
+    {
+      source = {
+        Gradient = { 
+          colors = { '#000000' }
+        }
+      },
+      width = '100%',
+      height = '100%',
+      opacity = 0.6
+    },
+}
+
+-- Main behavior
+config.window_close_confirmation = 'NeverPrompt'
+config.window_decorations = 'RESIZE'
+
+config.keys = keys
+
+return config
