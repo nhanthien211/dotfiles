38816f660f861739c93bea7c3e38488a76f119df
Nhan Tran <ttn2101@gmail.com>
2024-12-02 15:02:01 +0700

    Add wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,16 +11,31 @@ config.font = wezterm.font_with_fallback {
 config.font_size = 13
 config.freetype_load_target = 'Light'
 config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
 config.hide_tab_bar_if_only_one_tab = true
 config.warn_about_missing_glyphs = true
+
+config.window_padding = {
+  left = "0.50cell",
+  right = "0.5cell",
+  top = "0.5cell",
+  bottom = "0cell",
+}
+config.underline_thickness = "200%"
+config.underline_position = "-3pt"
+config.enable_scroll_bar = false
 config.animation_fps = 60
 config.prefer_egl = true
 config.debug_key_events = true
+config.inactive_pane_hsb = {
+  saturation = 0.5,
+  brightness = 0.2
+}
 config.background = {
   -- the back-most layer, should be image
   {
     source = {
-      File = '/Users/ttn/.config/wezterm/wallpaper/neovim-background-2.jpg',
+      File = '/Users/ttn/.config/wezterm/wallpaper/neovim-background-6.jpg',
     },
     opacity = 1.0,
   },
@@ -34,7 +49,7 @@ config.background = {
     },
     width = '100%',
     height = '100%',
-    opacity = 0.6
+    opacity = 0.5
   },
 }
 
@@ -44,17 +59,34 @@ config.enable_wayland = true
 
 -- Keys
 config.keys = {
-  { key = "%",   mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { key = "$",   mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
-  { key = "w",   mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane { confirm = true } },
+  -- Pane
+  { key = "$",          mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "%",          mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "x",          mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane { confirm = true } },
+  { key = "z",          mods = "CTRL|SHIFT", action = wezterm.action.TogglePaneZoomState },
+  { key = "h",          mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
+  { key = "l",          mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
+  { key = "k",          mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "j",          mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+  { key = "LeftArrow",  mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+  { key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+  { key = "UpArrow",    mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+  { key = "DownArrow",  mods = "CTRL|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
 
-  { key = "h",   mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
-  { key = "l",   mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
-  { key = "k",   mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
-  { key = "j",   mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+  -- Tab binding
+  { key = "t",          mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+  { key = "s",          mods = "CTRL|SHIFT", action = wezterm.action.ShowTabNavigator },
 
-  { key = "t",   mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-  { key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
+  -- Workspace
+  { key = "m",          mods = "CTRL|SHIFT", action = wezterm.action.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+
+  -- General
+  { key = "p",          mods = "CTRL|SHIFT", action = wezterm.action.ActivateCommandPalette },
+  { key = "y",          mods = "CTRL|SHIFT", action = wezterm.action.ActivateCopyMode },
+  { key = "c",          mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+  { key = "v",          mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+  { key = "h",          mods = "CTRL|SHIFT", action = wezterm.action.HideApplication },
+  { key = "f",          mods = "CTRL|SHIFT", action = wezterm.action.Search({ CaseSensitiveString = "" }) },
 }
 
 return config
