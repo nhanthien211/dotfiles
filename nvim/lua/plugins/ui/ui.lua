d9d6e0a3f92c19b4d9fa025a5eabfeac8a52ea7e
Nhan Tran <ttn2101@gmail.com>
2025-02-04 14:11:03 +0700

    Update ui config

--- nvim/lua/plugins/ui/ui.lua
+++ nvim/lua/plugins/ui/ui.lua
@@ -4,7 +4,15 @@ return {
     "folke/todo-comments.nvim",
     event = "VeryLazy",
     config = function()
-      require("todo-comments").setup()
+      require("todo-comments").setup({
+        keywords = {
+          TODO = { color = '#8caeee' },
+          PERF = { icon = "", color = "#cebebe", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = '', color = "#81c8be", alt = {} },
+          INFO = { icon = '󰋼', color = "#ca9ee6" },
+          HACK = { icon = "󱜢", }
+        }
+      })
     end,
   },
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -49,7 +49,7 @@ config.background = {
     },
     width = '100%',
     height = '100%',
-    opacity = 0.5
+    opacity = 0.7
   },
 }
 
